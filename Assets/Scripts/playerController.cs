af1cb8320e0bfd840e2285b6eb1f93db859dccf8
Catherine Taylor <cat.taylor.taylor@btinternet.com>
2020-07-10 22:13:06 +0100

    Renamed move player script

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -4,7 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class movePlayer : MonoBehaviour
+public class playerController : MonoBehaviour
 {
 
     /* CONTROLS
